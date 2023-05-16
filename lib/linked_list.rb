29de9a2a53f41b04df840f5cb31a3cd5aad06b73
Terry Lamb <terrylamb@gmail.com>
2023-05-16 19:28:30 -0400

    Add test files and code

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,4 +4,19 @@ class LinkedList
     def initialize
         @head = nil
     end
+
+    def append(data)
+       @head = Node.new(data)
+    end
+
+    def count
+        data_counter = 0
+        current_node = @head
+        if @head == nil
+            data_counter = 0
+        elsif current_node.next_node == nil
+            data_counter = 1
+        end
+    end
+    # count - tells us how many things are in the list. Count will start at the head of list and count nodes until it reaches nil.
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,10 +1,34 @@
-require "./lib/linked_list"
-require "./lib/node"
+require './lib/linked_list'
+require './lib/node'
 
 RSpec.describe LinkedList do
+    before(:each) do
+        @list = LinkedList.new
+      end
     it 'exists' do
-        list = LinkedList.new
-
-        expect(list.head).to eq(nil)
+    
+        expect(@list.head).to eq(nil)
     end
+
+    it "will append" do
+        
+        @list.append("doop")
+
+        expect(@list.head.data).to eq("doop")
+        expect(@list.head.next_node).to eq(nil)
+    end
+
+    it "will call a list" do
+
+        expect(@list).to be_instance_of(LinkedList)
+    end
+
+    it "will count the list" do
+        
+        @list.append("doop")
+        expect(@list.count).to eq(1)
+    end
+    
+
+    # list.count
 end
